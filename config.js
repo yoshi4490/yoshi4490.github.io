19417b403d4b831c4b916b5f15363c39f8fa116a
yoshi4490 <naxtuudaisuki@gmail.com>
2025-08-10 18:40:40 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // ←ここだけ！2行を書き換えたら保存（未設定でも動作はします・保存はローカルのみ）
-export const SUPABASE_URL = https://supabase.com/dashboard/project/yuazfkhebghlbotfvmze;   // 例: https://abcd1234.supabase.co
-export const SUPABASE_ANON_KEY = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl1YXpma2hlYmdobGJvdGZ2bXplIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ4MTM2NDQsImV4cCI6MjA3MDM4OTY0NH0.pEJkkbT6pPW__Sv0Zjhe2igCk5YtkT6c1HU9_wogDcE; // 例: eyJhbGciOi...（anon）
+export const SUPABASE_URL = "https://yuazfkhebghlbotfvmze.supabase.co";   // プロジェクトのAPI URL
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl1YXpma2hlYmdobGJvdGZ2bXplIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ4MTM2NDQsImV4cCI6MjA3MDM4OTY0NH0.pEJkkbT6pPW__Sv0Zjhe2igCk5YtkT6c1HU9_wogDcE"; // anonキー
