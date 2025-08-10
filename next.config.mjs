1a56c749872b0316070bd8bd1ea523485405ccbb
yoshi4490 <naxtuudaisuki@gmail.com>
2025-08-10 21:32:57 +0900

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: { serverActions: true },
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
-};
-export default nextConfig;
